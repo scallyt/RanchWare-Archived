6408508f99987e3ecbcc102039cffa131c88ce0c
scallyt <tnorbert201@gmail.com>
2024-02-24 18:59:16 +0100

    Sveltekit + Yarn
    I change the package manager from npm to yarn.
    + I switched from React to SvelteKIT

--- apps/ranchware-client/src/types/router.type.ts
+++
@@ -1,5 +0,0 @@
-import { RouteObject } from 'react-router-dom';
-export type Route = RouteObject & {
-  title?: string | undefined;
-  children?: Route[];
-};
--- apps/ranchware-client/vite.config.ts
+++ apps/ranchware-client/vite.config.ts
@@ -1,25 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 import commonjs from "@rollup/plugin-commonjs" 
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      plugins: [commonjs()]
-    },
-  },
-  optimizeDeps: {
-    include: ['api-contract/**/*']
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost3200', // Local Nest-App
-        changeOrigin: true
-      },
-    },
-  },
-})
-
+	plugins: [sveltekit()],
+	
+	build: {
+		rollupOptions: {
+		  plugins: [commonjs()]
+		},
+	  },
+	  optimizeDeps: {
+		include: ['api-contract/**/*']
+	  },
+	  server: {
+		proxy: {
+		  '/api': {
+			target: 'http://localhost3200', // Local Nest-App
+			changeOrigin: true
+		  },
+		},
+	  },
+});
