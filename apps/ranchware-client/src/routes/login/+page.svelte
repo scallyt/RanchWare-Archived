c5547fafce70dd090b0f03a54909e78a2f79a34f
scallyt <tnorbert201@gmail.com>
2024-02-25 19:50:02 +0100

    Login + Register page with no style + BE cors enabled
    HU:
    A stílus össze vissza a tokent nem menti el

---
+++ apps/ranchware-client/src/routes/login/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import axios from 'axios';
+
+	function submitForm(event: any) {
+		event.preventDefault();
+
+		const formData = new FormData(event.target);
+
+		const email = formData.get('email');
+        const pass = formData.get('pass');
+
+
+		axios.post('http://localhost:3100/api/auth/signin', {
+            email: email,
+            password: pass,
+		}).then(function (response) {
+            console.log(response)
+        }).catch( function (error) {
+            console.log(error);
+        })
+	}
+</script>
+
+<h1>Login</h1>
+
+<main>
+	<div>
+		<form on:submit|preventDefault={submitForm} action="/login">
+			<div>
+				<label for="email">email</label>
+				<input type="email" name="email" />
+			</div>
+			<div>
+				<label for="password">password</label>
+				<input min="8" type="password" name="pass" />
+			</div>
+			<button type="submit">Login</button>
+		</form>
+	</div>
+        <a href="/register">Register</a>
+</main>
+
+<style lang="scss">
+	@import '../../styles/register.module.scss';
+</style>
---
+++ apps/ranchware-client/src/routes/register/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import axios from 'axios';
+
+	function submitForm(event: any) {
+		event.preventDefault();
+
+		const formData = new FormData(event.target);
+
+		const firstName = formData.get('firstName');
+		const email = formData.get('email');
+		const lastName = formData.get('lastName');
+        const pass = formData.get('pass');
+
+		// Process the data
+		console.log(`Name: ${firstName}`);
+		console.log(`Email: ${email}`);
+		console.log(`Last Name: ${lastName}`);
+
+		axios.post('http://localhost:3100/api/auth/signup', {
+			firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: pass,
+		}).then(function (response) {
+            console.log(response)
+        }).catch( function (error) {
+            console.log(error);
+        })
+	}
+</script>
+
+<h1>Register</h1>
+
+<main>
+	<div>
+		<form on:submit|preventDefault={submitForm} action="/login">
+			<div>
+				<label for="firstName">firstName</label>
+				<input maxlength="16" type="text" name="firstName" />
+			</div>
+			<div>
+				<label for="lastName">lastName</label>
+				<input maxlength="16" type="text" name="lastName" />
+			</div>
+			<div>
+				<label for="email">email</label>
+				<input type="email" name="email" />
+			</div>
+			<div>
+				<label for="password">password</label>
+				<input min="8" type="password" name="pass" />
+			</div>
+			<button type="submit">Register</button>
+		</form>
+	</div>
+        <a href="/login">Login</a>
+</main>
+
+<style lang="scss">
+	@import '../../styles/register.module.scss';
+</style>
